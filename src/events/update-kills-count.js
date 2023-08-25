f1b2b06b2c74ea34ba72deac7bdfb8c76de03d12
ellementul <ananev.ivan.ur@ya.ru>
2023-08-25 11:56:18 +0500

    Added new events

--- src/events/update-kills-count.js
+++ src/events/update-kills-count.js
@@ -1,10 +1,7 @@
 const { EventFactory, Types } = require('@ellementul/united-events-environment')
 const type = Types.Object.Def({
-  system: "TileMap",
-  entity: Types.Any.Def([
-    Types.Const.Def("WallsCount"),
-    Types.Const.Def("PlatformsCount")
-  ]),
+  system: "Characters",
+  entity: "KillsCount",
   state: Types.Index.Def(256*256)
 }, true) 
 module.exports = EventFactory(type)
