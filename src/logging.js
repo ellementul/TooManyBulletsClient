972290fdaa1754fa77f211a4bf3b0f22c11ceb8a
ellementul <ananev.ivan.ur@ya.ru>
2023-10-29 12:55:55 +0500

    Optimization and logging

--- src/logging.js
+++ src/logging.js
@@ -1,10 +1,27 @@
 function Logging() {
   return function (payload) {
-    switch(payload.message.access) {
-      case "Local":
-        console.log("Client", payload.message)
+    switch(payload.message.system) {
+      case "Timing":
+        // console.log(payload.message)
+        break
+      case "PlayersManagment":
+        // console.log(payload.message)
+        break
+      case "TileMap":
+        // console.log(payload.message)
+        break
+      case "Characters":
+        // console.log(payload.message)
+        break
+
+      case "Actions":
+        // console.log(payload.message)
+        break
+      case "World":
+        // console.log(payload.message)
         break
       default:
+        console.log(payload.message.system)
         null
     }
   }
